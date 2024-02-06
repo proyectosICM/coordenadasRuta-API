ad1ff9f3a6c757ec527ba9e5848d824a9eb8107f
Eduardo Aguilar <es123y123@gmail.com>
2024-02-06 09:45:35 -0500

    c

--- src/main/java/com/ICM/coordenadasRutaAPI/Controllers/DispositivosController.java
+++ src/main/java/com/ICM/coordenadasRutaAPI/Controllers/DispositivosController.java
@@ -70,7 +70,7 @@ public class DispositivosController {
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    @PutMapping("/prop/{id}")
+    @GetMapping("/prop/{id}")
     public ResponseEntity<DispositivosModel> actualizarDispositivoprop  (@PathVariable Long id, @RequestBody DispositivosModel dispositivosModel){
         DispositivosModel edispositivos = dispositivosService.actualizarDispositivoprop(id, dispositivosModel);
         if (edispositivos!=null){
